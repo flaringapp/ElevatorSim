d8b131bb8a3b6156dda5c185a2aaff73034cf2d2
Andrii Shpek <ashpek01@gmail.com>
2022-01-25 21:15:39 +0200

    Make weight comparable

--- elevator_core/src/main/java/com/flaringapp/elevator_core/common/Weight.kt
+++ elevator_core/src/main/java/com/flaringapp/elevator_core/common/Weight.kt
@@ -1,4 +1,17 @@
 package com.flaringapp.elevator_core.common
 
 @JvmInline
-value class Weight(val value: Float)
+value class Weight(val value: Float): Comparable<Weight> {
+
+    override fun compareTo(other: Weight): Int {
+        return value.compareTo(other.value)
+    }
+
+    operator fun plus(other: Weight): Weight {
+        return Weight(value + other.value)
+    }
+
+    operator fun minus(other: Weight): Weight {
+        return Weight(value - other.value)
+    }
+}
