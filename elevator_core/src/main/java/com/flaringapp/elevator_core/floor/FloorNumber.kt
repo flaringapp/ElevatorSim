40a93925eadfdddceb751019779e24c0c875aedd
Andrii Shpek <ashpek01@gmail.com>
2022-01-25 22:08:01 +0200

    Implement plus/minus operations with floor number

--- elevator_core/src/main/java/com/flaringapp/elevator_core/floor/FloorNumber.kt
+++ elevator_core/src/main/java/com/flaringapp/elevator_core/floor/FloorNumber.kt
@@ -6,4 +6,21 @@ value class FloorNumber(val value: Int) : Comparable<FloorNumber> {
     override fun compareTo(other: FloorNumber): Int {
         return value.compareTo(other.value)
     }
+
+    operator fun plus(other: FloorNumber): FloorNumber {
+        return FloorNumber(value + other.value)
+    }
+
+    operator fun minus(other: FloorNumber): FloorNumber {
+        return FloorNumber(value - other.value)
+    }
+
+
+    operator fun plus(amount: Int): FloorNumber {
+        return FloorNumber(value + amount)
+    }
+
+    operator fun minus(amount: Int): FloorNumber {
+        return FloorNumber(value - amount)
+    }
 }
